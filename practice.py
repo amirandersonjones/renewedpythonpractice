a0c1d89a16789f364689d48de68b7babda834f28
amirandersonjones <amirandersonjones@gmail.com>
2022-07-26 22:14:54 -0500

    first

---
+++ practice.py
@@ -0,0 +1,102 @@
+# temperature = 20
+# name = "Batman"
+
+# if temperature > 30:
+#     print("It's not that bad outside")
+#     if temperature < 50:
+#         print("it could be warmer")
+#     elif temperature > 50:
+#         print("Thats what we cann nice weather in Minnesotta")
+
+# elif temperature > 20:
+#     if temperature < 32:
+#         print("Its still freezing outside")
+#     elif temperature > 32:
+#         print("well, at least snow will melt!")
+
+# elif temperature < 10:
+#     if temperature < 0:
+#         print("Why do I live here")
+#         print("Like no joke, on time it went from -60 to 40 degrees in one night")
+#         print("That's a 100 degree swing in one day, man Texas is looking good right now")
+
+# elif temperature == 20:
+#     print("Gee Willkers" + name + ", why do people live in Minnesota")
+
+# else: 
+#     print("This line is used to catch errors")
+
+# count_list = list(range(1,27, 2))
+# print(count_list)
+
+animals = ["dog", "cat", "snake", "goat"]
+# print(len(animals))
+
+# for animal in animals:
+#     print(animal)
+
+# for i in range(len(animals)):
+#     print(i, animals[i])
+
+# print(animals[2])
+
+# animals.append("mouse")
+# print(animals)
+# animals.insert(1,"mouse")
+# print(animals)
+
+# while "mouse" in animals:
+#     animals.remove("mouse")
+#     print(animals)
+# # print(animals)
+
+# list = ["thing1","thing2","thing3"]
+# for item in list:
+#     print(f" This {item} is in my list") 
+
+# portList= ["Interface g0/0", "Interface, g0/1", "Interface g0/2"]
+# deviceList = ["Catalyst 3650", "Catalyst 3750", "Catalyst 3850"]
+
+# for port in portList:
+#     for device in deviceList:
+#         print(f"{device}\n {port}\n shut\n")
+
+# names = ["Kali", "George", "Bob", "Mack"]
+# for i in range(len(names)):
+#     print(i, names[1])
+
+# evens_list = list(range(0,10, 2))
+# print(evens_list)
+# print(len(evens_list))
+
+# smoresIngredients = ["Chocolate", "Marshmellows", "Graham Crackers"]
+# for ingredient in smoresIngredients:
+#     if ingredient == "Marshmellows":
+# #         continue
+# #     print(ingredient)
+
+# smoresIngredients = ["Chocolate", "Marshmellows", "Graham Crackers"]
+# for ingredient in smoresIngredients:
+#     if ingredient == "Marshmellows":
+#         break
+#     print(ingredient)
+
+
+# print("end")
+
+# number = 5
+
+# while number < 20:
+#     print(f"This {number} is less than 10")
+#     number +=1
+
+#     if number == 15:
+#         break
+
+# login = input("Enter a login name ")
+
+# while len(login) < 8:
+#     print("Your login must be longer than 8 characters")
+#     login = input("Enter a login name")
+
+# print(f"Your login name is {login}")
